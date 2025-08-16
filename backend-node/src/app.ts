acec1fb10ba983d592fb58086f507338e5797dad
Marij <muhammadmarijahmed@gmail.com>
2025-08-17 01:36:24 +0500

    add redis adapter to sockets

--- backend-node/src/app.ts
+++ backend-node/src/app.ts
@@ -1,11 +1,36 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
+import { createAdapter } from "@socket.io/redis-adapter";
 import { router } from "./routes/routes";
 import { Server } from "socket.io";
 import jwt from 'jsonwebtoken'
 import http from 'http'
+import Redis from 'ioredis'
 dotenv.config()
+
+
+const pubClient = new Redis(process.env.REDIS_URI!);
+const subClient = pubClient.duplicate();
+
+
+
+
+
+// async function redisTest() {
+//     await client.set('foo', 'bar');
+//     const res = await client.get('foo')
+//     if (res == 'bar') {
+//         console.log('redis working')
+//         console.log(res)
+//     }
+//     else {
+//         console.log('error in redis')
+//         process.exit(0)
+//     }
+// }
+
+// redisTest()
 
 const app = express()
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
@@ -21,7 +46,8 @@ export const io = new Server(httpServer, {
     cors: {
         credentials: true,
         origin: process.env.FRONTEND_URL
-    }
+    },
+    adapter: createAdapter(pubClient, subClient)
 })
 
 // io.use() middleware here that will check jwt and attach the userId from jwt to socket object and will call next ..
@@ -41,10 +67,8 @@ io.use((socket, next) => {
         socket.userId = decoded.userId || decoded.id
         socket.userEmail = decoded.email
 
-        console.log("Token verified for user:", socket.userEmail)
         next()
     } catch (error) {
-        console.log("Invalid token:", error)
         return next(new Error("Invalid token"))
     }
 
@@ -54,23 +78,34 @@ io.on("connection", (socket) => {
     // every socket initiated by the same userId will always join the same room, so user can open multiple tabs and all his sockets will be in one room. which is his ID.. also we can target a notif to user as we already know his userID
     socket.join(socket.userId as string)
 
-    console.log(`${socket.userEmail} joined the room ${socket.userId}`)
+    // console.log(`${socket.userEmail} joined the room ${socket.userId}`)
     socket.on("disconnect", (reason) => {
-        console.log(`${socket.userEmail} disconnected due to ${reason}`)
+        // console.log(`${socket.userEmail} disconnected due to ${reason}`)
 
     })
 
 })
 
-io.on("disconnect", (socket) => {
-    console.log(`${socket.userEmail} disconnected`)
-
-})
 
 
 
 
-const PORT = process.env.PORT || 3000
+async function shutdown() {
+    console.log("Shutting down...");
+    io.sockets.sockets.forEach((s) => s.disconnect(true));
+    await pubClient.quit().catch(() => { });
+    await subClient.quit().catch(() => { });
+    httpServer.close(() => process.exit(0));
+    setTimeout(() => process.exit(1), 8000).unref();
+}
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+
+
+
+const PORT = process.env.PORT
 
 httpServer.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
